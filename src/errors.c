f839ab7604b08fd90a9f33277543431c62bc5732
Vadim Jan Nieto <vjan-nie@c1r14s6.42madrid.com>
2025-06-10 11:49:20 +0200

    Error message added as asked in the subject

--- src/errors.c
+++ src/errors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vjan-nie <vjan-nie@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: vjan-nie <vjan-nie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 14:03:52 by vjan-nie          #+#    #+#             */
-/*   Updated: 2025/06/04 14:13:00 by vjan-nie         ###   ########.fr       */
+/*   Updated: 2025/06/10 11:45:44 by vjan-nie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	arg_error(char *error) //Errors in user's call: messange in terminal
 {
+	ft_printf("Error\n");
 	ft_printf("%s", error);
 	exit(1);
 }
@@ -22,12 +23,14 @@ void	map_error(char **map, char *error)
 {
 	if (map)
 		free_map(map);
+	ft_printf("Error\n");
 	ft_putstr_fd(error, 2);
 	exit(1);
 }
 
 void	ft_mlx_error(char **map)
 {
+	ft_printf("Error\n");
 	ft_putstr_fd("MLX initialization error\n", 2);
 	free_map(map);
 	exit(1);
@@ -35,6 +38,7 @@ void	ft_mlx_error(char **map)
 
 void	ft_window_error(void *mlx, char **map)
 {
+	ft_printf("Error\n");
 	ft_putstr_fd("Window creation error\n", 2);
 	free_map(map);
 	mlx_destroy_display(mlx);
@@ -43,6 +47,7 @@ void	ft_window_error(void *mlx, char **map)
 
 void	ft_img_error(t_game *game)
 {
+	ft_printf("Error\n");
 	ft_putstr_fd("Load image error\n", 2);
 	exit_game(game, 1);
 	exit(1);
